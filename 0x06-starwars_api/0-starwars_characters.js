f425e150cec9bf5ccefb099d79eeeb4324753aef
Moutaz Mahmoud <moutaz.prof.egy@gmail.com>
2024-11-13 13:11:12 +0200

    Refactor: some updates

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,46 +2,26 @@
 
 const request = require('request');
 
-// Get movie ID from the command line arguments
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-// URL for the Star Wars API with the specified movie ID
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// Fetch the movie data
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const movie = JSON.parse(body);
-  const characters = movie.characters;
-
-  // Helper function to fetch each character's name in sequence
-  function fetchCharacter(index) {
-    if (index >= characters.length) {
-      return; // All characters have been fetched
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
     }
+  });
+};
 
-    request(characters[index], (error, response, body) => {
-      if (error) {
-        console.error(error);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-
-      // Fetch the next character in sequence
-      fetchCharacter(index + 1);
-    });
+request(
+  `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-
-  // Start fetching characters from the first one
-  fetchCharacter(0);
-});
+);
